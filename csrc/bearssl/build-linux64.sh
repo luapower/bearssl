5edeb4bff16e0bed04b94cf093f76962d2809240
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2020-11-17 01:29:00 +0200

    unimportant

--- csrc/bearssl/build-linux64.sh
+++ csrc/bearssl/build-linux64.sh
@@ -1,3 +1,2 @@
-C="-fPIC mz_strm_os_posix.c mz_os_posix.c mz_crypt_brg.c brg/*.c -Ibrg" \
-	P=linux64 L="-s -static-libgcc -Wl,--unresolved-symbols=report-all" \
-	D=libminizip2.so A=libminizip2.a ./build.sh
+P=linux64 L="-s -static-libgcc" \
+	D=libbearssl.so A=libbearssl.a ./build.sh
